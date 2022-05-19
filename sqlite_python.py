037a1d298bef6b81149947f2a0bf628d5b7fafaa
Jon <jonathan.stott@gmail.com>
2022-05-19 15:23:40 +0100

    Add function docs

--- sqlite_python.py
+++ sqlite_python.py
@@ -2,6 +2,16 @@ import sqlite3
 
 
 def db_connect(database_name: str) -> sqlite3.Connection:
+    """
+    Helper function to connect to sqlite3 from python
+
+    This function mostly exists to set the `row_factory` option to
+    allow 'string' or index based access of row elements and also
+    to set the important PRAGMA foreign_keys to ON, which means
+    the database will enforce foreign key constraints (an important
+    data integrity measure across tables)
+
+    """
     db = sqlite3.connect(
         database_name,
         detect_types=sqlite3.PARSE_DECLTYPES,
