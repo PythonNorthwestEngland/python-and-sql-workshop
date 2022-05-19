997a3a28827f0703f23329cdb90e228e108b5820
Jon <jonathan.stott@gmail.com>
2022-05-19 20:01:37 +0100

    Add example showing querying, inserting
    And transactions!

--- sqlite_python.py
+++ sqlite_python.py
@@ -1,4 +1,7 @@
+from builtins import RuntimeError
 import sqlite3
+
+USER_ID = 100000
 
 
 def db_connect(database_name: str) -> sqlite3.Connection:
@@ -24,4 +27,141 @@ def db_connect(database_name: str) -> sqlite3.Connection:
     return db
 
 
+# connect to the database
 conn = db_connect("books.db")
+
+# Show there are no python books :(
+cur = conn.execute(
+    """
+SELECT
+book_id, title, authors, original_publication_year
+FROM books
+WHERE title like '%' || :title || '%' -- this allows safe, parameterised, wildcard queries
+ORDER BY original_publication_year
+LIMIT :limit
+""",
+    {"title": "python", "limit": 10},
+)
+for row in cur:
+    print(f"{row['book_id']} {row['title']}")
+
+print("== Inserting book==============================================")
+
+# insert a python book successfully
+# Also, insert this to our to_read pile
+with conn:
+    cur = conn.execute(
+        """INSERT INTO books(authors, title, original_publication_year, isbn13)
+                     VALUES (:authors, :title, :original_publication_year, :isbn13)
+         returning book_id""",
+        {
+            "authors": "Harry J.W. Percival, Bob Gregory",
+            "title": "Architecture Patterns With Python",
+            "original_publication_year": 2020,
+            "isbn13": "9781492052203",
+        },
+    )
+
+    book_id = cur.fetchone()[0]
+    print(f"New book id: {book_id}")
+
+    cur = conn.execute(
+        """INSERT INTO to_read(book_id, user_id)
+                     VALUES (:book_id, :user_id)
+         """,
+        {
+            "book_id": book_id,
+            "user_id": USER_ID,
+        },
+    )
+
+    cur = conn.execute(
+        """
+        SELECT * FROM to_read WHERE user_id = :user_id
+        """,
+        {"user_id": USER_ID},
+    )
+    print("to_read table (after the first insert):")
+    for row in cur:
+        print(f"{row['book_id']=} {row['user_id']=}")
+
+print("== Inserting book with an error ========================================")
+
+
+# insert a python book
+# Also, insert this to our to_read pile
+# but then we fail due to a RuntimeError :(
+try:
+    with conn:
+        cur = conn.execute(
+            """
+            INSERT INTO books(authors, title, original_publication_year)
+                    VALUES (:authors, :title, :original_publication_year)
+            returning book_id
+            """,
+            {
+                "authors": "Luciano Ramalho",
+                "title": "Fluent Python",
+                "original_publication_year": 2015,
+            },
+        )
+        book_id = cur.fetchone()[0]
+        print(f"New book id: {book_id}")
+
+        # use this in our to_read insert statement
+        cur = conn.execute(
+            """INSERT INTO to_read(book_id, user_id)
+                     VALUES (:book_id, :user_id)
+         """,
+            {
+                "book_id": book_id,
+                "user_id": USER_ID,
+            },
+        )
+
+        cur = conn.execute(
+            """
+        SELECT * FROM to_read WHERE user_id = :user_id
+        """,
+            {"user_id": USER_ID},
+        )
+        print("to_read table (just before the error, still in the transaction):")
+        for row in cur:
+            print(f"{row['book_id']=} {row['user_id']=}")
+        print("Both our books are in the to read pile")
+
+        raise RuntimeError("Stop transaction")
+
+except Exception:
+    print("Ooops, we failed!")
+
+print("== Querying after the error, outside the transaction =======================")
+
+# Onlyone row in the to_read table
+cur = conn.execute(
+    """
+SELECT * FROM to_read WHERE user_id = :user_id
+""",
+    {"user_id": USER_ID},
+)
+print("to_read table (just after the error):")
+for row in cur:
+    print(f"{row['book_id']=} {row['user_id']=}")
+
+# Show there is a python book!
+cur = conn.execute(
+    """
+SELECT
+book_id, title, authors, original_publication_year
+FROM books
+WHERE title like '%' || :title || '%' -- this allows safe, parameterised, wildcard queries
+ORDER BY original_publication_year
+LIMIT :limit
+""",
+    {"title": "python", "limit": 10},
+)
+for row in cur:
+    print(f"{row['book_id']} {row['title']} {row['authors']}")
+
+
+conn.close()
